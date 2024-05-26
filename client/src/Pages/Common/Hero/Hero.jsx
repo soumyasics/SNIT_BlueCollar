bc8577ddde7e0f20914d1a6eb9dfaa0af31de838
Jithin Jose <jithinjose.sics@gmail.com>
2024-05-26 14:43:17 +0530

    navbar and homepage modified

--- client/src/Pages/Common/Hero/Hero.jsx
+++ client/src/Pages/Common/Hero/Hero.jsx
@@ -1,39 +1,55 @@
 import React from 'react';
-import './Hero.css'
+import { Container, Row, Col, Form, Button, InputGroup, Dropdown, DropdownButton } from 'react-bootstrap';
+import './Hero.css';
 
 function Hero() {
   return (
-        <div className='home-hero'>
-              <div className='home-hero-heading mx-5 p-5 mb-5'>
-                  Get Your Hands Dirty on Your Terms: Find Blue-Collar Jobs Now!
-              </div>
-              <div className="container text-center">
-                  <div className="row">
-                      <div className="col mx-2">
-                          <div className="input-group mb-3">
-                              <input type="text" className="form-control" placeholder="Job Name" aria-label="JobName" aria-describedby="basic-input" />
-                          </div>
-                      </div>
-                      <div className="col mx-2">
-                          <div className="input-group">
-                              <input type="text" className="form-control" aria-label="Text input dropdown button" placeholder='Category' />
-                              <button className="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
-                              <ul className="dropdown-menu dropdown-menu-end w-100">
-                                  <li><a className="dropdown-item" href="#">Category1</a></li>
-                                  <li><a className="dropdown-item" href="#">Category2</a></li>
-                                  <li><a className="dropdown-item" href="#">Category3</a></li>
-                              </ul>
-                          </div>
-                      </div>
-                      <div className="col mx-2 ">
-                          <div className="d-grid gap-2">
-                              <button type="button" className="btn btn-warning text-white fs-6">Search Job</button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+    <div className='home-hero'>
+      <div className='home-hero-heading mx-5 p-5 mb-5'>
+        Get Your Hands Dirty on Your Terms: Find Blue-Collar Jobs Now!
+      </div>
+      <Container className="text-center">
+        <Row>
+          <Col className="mx-1">
+            <InputGroup className="mb-3">
+              <Form.Control
+                type="text"
+                placeholder="Job Name"
+                aria-label="JobName"
+                aria-describedby="basic-input"
+                className="custom-placeholder py-2"
+              />
+            </InputGroup>
+          </Col>
+          <Col className="mx-1">
+            <InputGroup>
+              <Form.Control
+                type="text"
+                placeholder="Category"
+                aria-label="Text input dropdown button"
+                className="custom-placeholder py-2"
+              />
+              <DropdownButton
+                variant="light"
+                title=""
+                id="input-group-dropdown-2"
+                align="end"
+              >
+                <Dropdown.Item href="#">Category1</Dropdown.Item>
+                <Dropdown.Item href="#">Category2</Dropdown.Item>
+                <Dropdown.Item href="#">Category3</Dropdown.Item>
+              </DropdownButton>
+            </InputGroup>
+          </Col>
+          <Col className="mx-1">
+            <div className="d-grid gap-1">
+              <Button variant="warning" className="text-white fs-6 py-2">Search Job</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
-export default Hero
+export default Hero;
