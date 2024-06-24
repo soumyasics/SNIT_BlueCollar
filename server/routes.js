586225a84a809af9f5a13b144ccf4a6912ab6747
vinayak-srishti <vinayabr.sics@gmail.com>
2024-06-24 10:38:36 +0530

    updated

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,8 @@ const customer=require("./models/customer/customerController")
 const Worker=require("./models/Worker/workerController")
 const employer=require("./models/Employer/employerController")
 
+// Customer
+
 router.post("/registercust",customer.upload,customer.registercust)
 router.post("/logincust",customer.custLogin)
 router.post("/verifytoken",customer.verifyToken)
@@ -15,14 +17,29 @@ router.post("/viewcustbyid/:id",customer.viewcustbyid)
 router.post("/deletecust/:id",customer.deletecustById)
 router.post("/forgotPWDsentMail",customer.forgotPWDsentMail)
 
+//Worker
 
 router.post("/registerworker",Worker.upload,Worker.registerworker)
 router.post("/loginworker",Worker.workerLogin)
 router.post("/workerresetpswd/:id",Worker.workerresetpswd)
+router.post("/updateworkerprofile/:id",Worker.upload,Worker.updateworkerprofile)
+router.post("/viewallworker",Worker.viewallworker)
+router.post("/viewworkerbyid/:id",Worker.viewworkerbyid)
+router.post("/deleteworkerById/:id",Worker.deleteworkerById)
 
+
+//Employer
 
 router.post("/employerreg",employer.upload,employer.registeremp)
 router.post("/emplogin",employer.empLogin)
 router.post("/employerresetpswd/:id",employer.employerresetpswd)
+router.post("/updateempprofile/:id",employer.upload,employer.updateempprofile)
+router.post("/viewallemployer",employer.viewallemployer)
+router.post("/viewempbyid/:id",employer.viewempbyid)
+router.post("/deleteempById/:id",employer.deleteempById)
+
+
+
+
 
 module.exports=router
