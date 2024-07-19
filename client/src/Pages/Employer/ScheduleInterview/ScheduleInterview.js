0f2f44cd89b03c432cff680972459c81370a9bb2
bijo <bijoksanoai@gmail.com>
2024-07-19 17:39:42 +0530

    bijo_blue-collar_19.7.24_3

--- client/src/Pages/Employer/ScheduleInterview/ScheduleInterview.js
+++ client/src/Pages/Employer/ScheduleInterview/ScheduleInterview.js
@@ -69,9 +69,7 @@ function ScheduleInterview({close}) {
     
         if (Object.keys(errors).length === 0 && formValid) {
           
-    
-          
-          try {
+        try {
             var response;
             if (data) {
               response = await axiosInstance.post(
@@ -108,7 +106,7 @@ function ScheduleInterview({close}) {
                 <path d="m20.48 3.512c-2.172-2.171-5.172-3.514-8.486-3.514-6.628 0-12.001 5.373-12.001 12.001 0 3.314 1.344 6.315 3.516 8.487 2.172 2.171 5.172 3.514 8.486 3.514 6.628 0 12.001-5.373 12.001-12.001 0-3.314-1.344-6.315-3.516-8.487zm-1.542 15.427c-1.777 1.777-4.232 2.876-6.943 2.876-5.423 0-9.819-4.396-9.819-9.819 0-2.711 1.099-5.166 2.876-6.943 1.777-1.777 4.231-2.876 6.942-2.876 5.422 0 9.818 4.396 9.818 9.818 0 2.711-1.099 5.166-2.876 6.942z"/><path d="m13.537 12 3.855-3.855c.178-.194.287-.453.287-.737 0-.603-.489-1.091-1.091-1.091-.285 0-.544.109-.738.287l.001-.001-3.855 3.855-3.855-3.855c-.193-.178-.453-.287-.737-.287-.603 0-1.091.489-1.091 1.091 0 .285.109.544.287.738l-.001-.001 3.855 3.855-3.855 3.855c-.218.2-.354.486-.354.804 0 .603.489 1.091 1.091 1.091.318 0 .604-.136.804-.353l.001-.001 3.855-3.855 3.855 3.855c.2.218.486.354.804.354.603 0 1.091-.489 1.091-1.091 0-.318-.136-.604-.353-.804l-.001-.001z"/></svg>
 			</button>
                 </div>
-                
+                <form onSubmit={(e)=>{handleSubmit(e);}}>
                 <div className='empviewpostjob-con'>
                 <Row>
                     <Col><label className='empviewpostjob-label mb-2'>Interview Date</label></Col>
@@ -122,6 +120,7 @@ function ScheduleInterview({close}) {
                     name='interview_date'
                     onChange={handleChange}
                     />
+                    {errors &&<p className='text-danger'>{errors.interview_date}</p>}
                     </Col>
                     <Col>
                     <input className='empviewpostjob-input' 
@@ -130,6 +129,7 @@ function ScheduleInterview({close}) {
                     name='interview_location'
                     onChange={handleChange}
                     />
+                    {errors &&<p className='text-danger'>{errors.interview_location}</p>}
                     </Col>
                 </Row>
                 <Row>
@@ -144,6 +144,7 @@ function ScheduleInterview({close}) {
                     name='city'
                     onChange={handleChange}
                     />
+                    {errors &&<p className='text-danger'>{errors.city}</p>}
                     </Col>
                     <Col>
                     <input className='empviewpostjob-input' 
@@ -152,12 +153,14 @@ function ScheduleInterview({close}) {
                     name='state'
                     onChange={handleChange}
                     />
+                    {errors &&<p className='text-danger'>{errors.state}</p>}
                     </Col>
                 </Row>
                 <div className='empviewpostjob-schedule-div'>
                     <button className='empviewpostjob-schedule'>Schedule</button>
                 </div>
                 </div>
+                </form>
             </div>
         </div>
 
